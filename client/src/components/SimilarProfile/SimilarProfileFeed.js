49ec6d76672c061d835695f99f97d5c9853a9f85
zeke <zeke.eteveuni@gmail.com>
2024-01-21 06:40:17 -0800

    edited similarprofile && feed

--- client/src/components/SimilarProfile/SimilarProfileFeed.js
+++ client/src/components/SimilarProfile/SimilarProfileFeed.js
@@ -1,14 +1,37 @@
 import React from 'react';
 import SimilarProfile from './SimilarProfile'
-// import postData from
+import "./SimilarProfileFeed.css"
+import { useState } from 'react';
+import red1 from '../../pics/red1.png';
+import red2 from '../../pics/red2.png';
+import red3 from '../../pics/red3.png';
+import red4 from '../../pics/red4.png';
+import red5 from '../../pics/red5.png';
+import red6 from '../../pics/red6.png';
 
-const SimilarProfileFeed = () => {
+
+const SimilarProfileFeed = (props) => {
+    const images = [red1, red2, red3, red4, red5, red6];
+    const [currentIndex, setCurrentIndex] = useState([0,0,0,0,0,0]);
+
+    function pickAnImage() {
+        const index = Math.floor(Math.random() * images.length);
+        if (currentIndex[index] !== 0) {
+            pickAnImage();
+        }
+
+        setCurrentIndex( currentIndex[index] = 1)   // not available anymore
+        return images[index];
+    }
+
+
+    const image = pickAnImage();
 
     return (
-        <div>
-        {/*     {postData.posts.map((post, index) => (*/}
-        {/*        <SimilarProfile key={index} post={post}/>*/}
-        {/*))}*/}
+        <div className = "similarProfileFeed">
+             {props.data.map((post, index) => (
+                <SimilarProfile key={index} post={post} image={image}/>
+        ))}
         </div>
     );
 };
