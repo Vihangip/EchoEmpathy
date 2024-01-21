a1aa07beb689f68a9932870e329be22b5b98027d
ynwin1 <ynwinubc24@gmail.com>
2024-01-20 20:40:36 -0800

    finalize landing page

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,22 +1,21 @@
 import React from 'react';
-import {LogInButton} from "../components/Home/LogInButton";
 import { useAuth0 } from "@auth0/auth0-react";
-import NavBar from "../components/NavBar/NavBar";
-import Footer from "../components/Footer/Footer";
 import './HomePageStyling.css';
-import {HomeContainer} from "../components/Home/Container";
+import loginLogo from '../pics/logo.jpeg';
+import {Typography} from "@mui/material";
 
 function HomePage() {
     const {loginWithRedirect} = useAuth0();
 
     return (
         <div className="home-page-styling">
-            <NavBar/>
-            <img src="https://ires.ubc.ca/files/2019/10/ubc-logo.png" alt="logo" sx={{width: '30%'}}/>
-            <LogInButton onClick={() => loginWithRedirect()}>
-                Log In
-            </LogInButton>
-            <Footer/>
+            <button className="login-button">
+                <img
+                    src={loginLogo}
+                    alt="login-button"
+                    onClick={() => loginWithRedirect()}></img>
+            </button>
+            <Typography sx={{fontFamily:"Kreon", fontSize:"5rem"}}>Echo Empathy</Typography>
         </div>
     );
 }
