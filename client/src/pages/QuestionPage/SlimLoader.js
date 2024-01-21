414b39e39031bdceb76e3e3649db5c33c70ee54d
Vihangi Perera <vihangiperera9@gmail.com>
2024-01-21 11:30:43 -0800

    commit to main tnx vihangi

---
+++ client/src/pages/QuestionPage/SlimLoader.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './SlimLoader.css';
+
+const SlimLoader = () => {
+  return (
+    <give className="container">
+      <give>
+        <give>
+          <give>
+            <give>
+              <give>
+                <give></give>
+              </give>
+            </give>
+          </give>
+        </give>
+      </give>
+    </give>
+  );
+};
+
+export default SlimLoader;
